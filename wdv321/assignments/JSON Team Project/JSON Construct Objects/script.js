52bcc203762eab4656a69a84bd6e20d58ad25a4f
Eric Burkheimer <eaburkheimer@dmacc.edu>
2025-11-17 13:39:41 -0600

    update team project

---
+++ wdv321/assignments/JSON Team Project/JSON Construct Objects/script.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+/*
+  This file will:
+  - Create a Javascript object
+  - Convert the Javascript object into a JSON object
+  - Store the JSON object into local storage
+
+  Goal: Provide an example of how to create JSON objects in Javascript
+  Goal: Provide an example of how to consume JSON objects in Javascript
+
+  Use the following data for your JSON object
+
+    student_id = 332443
+    student_gpa = 3.6
+    student_courses = ["WDV101","WDV131","WDV105"]
+
+*/
+
---
+++ wdv321/assignments/JSON Team Project/JSON Consume-Array of Objects/script.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+/*
+  This file will:
+
+  - Create a Javascript array containing a list of javascript objects, with each record including another array
+  - Convert the Javascript object into a JSON string
+  - Store the JSON string into local storage
+
+  Goal: Provide an example of how to create an array of javascript objects
+  Goal: Provide an example of how to consume a JSON string in JS
+
+  Use the following data for this:
+
+    student_id = 332443
+    student_gpa = 3.6
+    student_courses = ["WDV101","WDV131","WDV105"]
+
+    student_id = 545467
+    student_gpa = 2.7
+    student_courses = ["WDV101","WDV131","WDV105","WDV221","WDV205"]
+
+    student_id = 128574
+    student_gpa = 3.4
+    student_courses = ["WDV101","WDV131","WDV105","WDV221","WDV205","WDV341"]
+
+    student_id = 750056
+    student_gpa = 1.85
+    student_courses = ["WDV101","WDV131","WDV105","WDV221","WDV205"]
+*/
