e8fef54981ab6c407f96dbc2c22481a0d2ad8597
Eric Burkheimer <eaburkheimer@dmacc.edu>
2026-03-24 11:13:07 -0500

    updates for clarity

--- wdv321/assignments/Unit 5-1 - Fetch API/Template/js/main.js
+++ wdv321/assignments/Unit 5-1 - Fetch API/Template/js/main.js
@@ -1,12 +1,10 @@
-import { fetchData } from "./api";
-//add additional imports as needed
+//add additional imports here (functions from ui.js)
 
-// Create an event handler on the button to fetch the data.
-// This will NOT call the Fetch API directly, instead it will call the function defined in api.js
-// Overview of what the click handler will need to do:
-//   - Hint: this should be functions calls imported from api.js and ui.js
-//   1. Show a loading status
-//   2. Fetch the data - pass the value from the text box into the function
-//   3. Clear the loading status
-//   4. Render results
-//   5. Include Error handling - i.e. try/catch errors
+// Create a click event handler on the "Show Countries" button to fetch the data.
+// This will NOT call the Fetch API directly, instead it will call a function defined in api.js
+// Overview of what the click handler will need to do (this will be a series of function calls):
+//   - Show a loading status
+//   - Fetch the data
+//   - Clear the loading status
+//   - Render results
+//   - Include Error handling - i.e. try/catch errors
