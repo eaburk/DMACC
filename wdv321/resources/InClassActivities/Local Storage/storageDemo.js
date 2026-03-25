3fc6de935edfaba3838a00a37e9b7e906988cf74
Eric Burkheimer <eaburkheimer@dmacc.edu>
2026-03-25 13:11:30 -0500

    fixes

--- wdv321/resources/InClassActivities/Local Storage/storageDemo.js
+++
@@ -1,8 +0,0 @@
-const sbm = document.getElementById("submit");
-const nme = document.getElementById("name");
-
-sbm.addEventListener("click", () => {
-  localStorage.setItem("name", nme.value);
-  console.log('ls: ', JSON.stringify(localStorage));
-  console.log("From LS: ", localStorage.name);
-});
