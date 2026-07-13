1f5fd009356532f52a482637e9f89e1579e89263
Eric Burkheimer <eaburkheimer@dmacc.edu>
2026-07-13 10:05:05 -0500

    activity

---
+++ wdv221/resources/InClassActivities/Built In Objects/objects.js
@@ -0,0 +1,3 @@
+document.querySelector("#stringBtn").addEventListener("click", showStrings);
+document.querySelector("#styleBtn").addEventListener("click", changeStyle);
+document.querySelector("#dateBtn").addEventListener("click", showDate);
